2d664d0b1d8d20c7d613113109cbeca8f910b480
dejo <d.dejanovic@dvcsolutions.com>
2020-02-09 16:39:03 +0100

    changed db filers

--- client/src/Chat/ConversationsPage.js
+++ client/src/Chat/ConversationsPage.js
@@ -1,35 +1,72 @@
 import React, { Component } from "react";
 import { Button, Modal } from 'semantic-ui-react';
 import axios from 'axios';
+import { getConversations, deleteConversation, newConversation } from '../actions/conversationActions';
+import { connect } from 'react-redux';
+import store from '../store';
+import { Icon } from 'semantic-ui-react';
+import { DELETE_CONVERSATION, ADD_CONVERSATION } from "../actions/types";
 
 class ConversationsPage extends Component {
 
     constructor(props) {
         super(props);
-        console.log(props)
 
         this.state = {
             modalOpen: false,
             conversationUsers: []
         }
+
+        this.props.socket.on('add-conversation', conversation => {
+            store.dispatch({
+                type: ADD_CONVERSATION,
+                payload: conversation
+            })
+        });
+
+        this.props.socket.on('delete-conversation', conversationId => {
+            store.dispatch({
+                type: DELETE_CONVERSATION,
+                payload: conversationId
+            })
+        });
+    }
+
+    componentDidMount() {
+        store.dispatch(getConversations(this.props.token));
     }
 
     render() {
+        const { conversations } = this.props.conversation;
         return (
             <div>
                 <h4>Started conversations</h4>
 
-                <Modal size="mini" open={this.state.modalOpen} trigger={<Button onClick={this.openModal} primary>New Chat</Button>}>
+                {
+                    conversations.length > 0 
+                    ?
+                    conversations.map((conversation, i) => {
+                        return (
+                            <div key={`conv${i}`}>
+                                <p>{conversation._id} <Icon onClick={this.deleteConversation.bind(this, conversation._id)} link name='close' /></p>
+                            </div>
+                        );
+                    })
+                    :
+                    <p>There are no conversations.</p>
+                }
+
+                <Modal size="mini" open={this.state.modalOpen} trigger={<Button style={{marginTop: '20px'}} onClick={this.openModal} primary>New Chat</Button>}>
                     <Modal.Header>Start a conversation with:</Modal.Header>
                     <Modal.Content>
-                        {this.state.conversationUsers.map((user, index) => {
+                        {this.state.conversationUsers.length > 0 ? this.state.conversationUsers.map((user, index) => {
                             return (
                                 <div key={index} style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
                                     <p>{user.name}</p>
                                     <Button color='purple' size='mini' onClick={this.startNewConversation.bind(this, user._id)}>Chat</Button>
                                 </div>
                             )
-                        })}
+                        }) : <p>There are no users!</p>}
                     </Modal.Content>
                     <Modal.Actions>
                         <Button onClick={this.closeModal} negative>Close</Button>
@@ -59,17 +96,15 @@ class ConversationsPage extends Component {
     }
 
     startNewConversation = id => {
-        console.log(id)
-        axios.post('http://localhost:8080/api/conversations', JSON.stringify({id}), {
-            headers: {
-                "Content-Type": "application/json",
-                "token": this.props.token
-            }
-        })
-        .then(res => res.data).then(data => console.log(data))
-        .catch(err => console.log(err));
+        this.props.newConversation(this.props.token, id, this.props.socket);
+        this.closeModal();
+    }
+
+    deleteConversation = id => {
+        this.props.deleteConversation(this.props.token, id, this.props.socket)
     }
 
 }
 
-export default ConversationsPage;
+const mapStateToProps = (state) => ({ conversation: state.conversation });
+export default connect(mapStateToProps, { getConversations, deleteConversation, newConversation })(ConversationsPage);
--- client/src/actions/conversationActions.js
+++ client/src/actions/conversationActions.js
@@ -2,15 +2,50 @@ import { GET_CONVERSATIONS } from "./types";
 import axios from 'axios';
 
 export const getConversationUsers = token => dispatch => {
-    axios.get('http://localhost:8080/api/conversations/users', {
+    // axios.get('http://localhost:8080/api/conversations/users', {
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "token": token
+    //     }
+    // })
+    // .then(res => res.data).then(data => dispatch({
+    //     type: GET_MESSAGES,
+    //     payload: data
+    // }))
+    // .catch(err => console.log(err));
+};
+
+export const newConversation = (token, id, socket) => dispatch => {
+    console.log(id)
+    axios.post('http://localhost:8080/api/conversations', JSON.stringify({id}), {
+        headers: {
+            "Content-Type": "application/json",
+            "token": token
+        }
+    })
+    .then(res => res.data).then(data => socket.emit('add-conversation', data))
+    .catch(err => console.log(err));
+}
+
+export const getConversations = token => dispatch => {
+    axios.get('http://localhost:8080/api/conversations', {
         headers: {
             "Content-Type": "application/json",
             "token": token
         }
     })
     .then(res => res.data).then(data => dispatch({
-        type: GET_MESSAGES,
+        type: GET_CONVERSATIONS,
         payload: data
     }))
     .catch(err => console.log(err));
+}
+
+export const deleteConversation = (token, id, socket) => dispatch => {
+    axios.delete(`http://localhost:8080/api/conversations/${id}`, {
+        headers: {
+            "Content-Type": "application/json",
+            "token": token
+        }
+    }).then(res => res.data).then(data => socket.emit('delete-conversation', id))
 };
